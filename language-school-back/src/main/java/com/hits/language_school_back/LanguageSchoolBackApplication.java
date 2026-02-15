e310c2a22f56bbe17ccf197912a6d6a0bd97e578
Egor Morozov <egornexus10@gmail.com>
2026-02-15 15:37:51 +0700

    back init

---
+++ language-school-back/src/main/java/com/hits/language_school_back/LanguageSchoolBackApplication.java
@@ -0,0 +1,13 @@
+package com.hits.language_school_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageSchoolBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageSchoolBackApplication.class, args);
+	}
+
+}
