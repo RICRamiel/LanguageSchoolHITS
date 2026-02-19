5d26f94b5c21c51ca56ad66d8c1afa50d7efa6d5
Ramiel <tardis40type@gmail.com>
2026-02-19 22:05:58 +0700

    resolved version conflict with openapi swagger. Added & tested swagger generation.

--- language-school-back/src/main/java/com/hits/language_school_back/config/SwaggerConfig.java
+++ language-school-back/src/main/java/com/hits/language_school_back/config/SwaggerConfig.java
@@ -1,28 +1,28 @@
-//package com.hits.language_school_back.config;
-//
-//import io.swagger.v3.oas.models.Components;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import io.swagger.v3.oas.models.security.SecurityRequirement;
-//import io.swagger.v3.oas.models.security.SecurityScheme;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("Language School API")
-//                        .version("1.0"))
-//                .addSecurityItem(new SecurityRequirement().addList("JWT"))
-//                .components(new Components()
-//                        .addSecuritySchemes("JWT", new SecurityScheme()
-//                                .name("JWT")
-//                                .type(SecurityScheme.Type.HTTP)
-//                                .scheme("bearer")
-//                                .bearerFormat("JWT")));
-//    }
-//}
+package com.hits.language_school_back.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Language School API")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .name("JWT")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
