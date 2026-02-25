c10d1470016dd9cd1286dce476f6d6868d385cf5
Linberg34 <pfqxbr2208@gmail.com>
2026-02-25 22:43:32 +0700

    add notifications

--- language-school-app/src/app/pages/student-page/student-page.component.ts
+++ language-school-app/src/app/pages/student-page/student-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../shared/ui/header/header.component';
 import {TabsComponent} from '../../shared/ui/tabs/tabs.component';
 import {TaskCardComponent} from '../../shared/ui/task-card/task-card.component';
+import {NotificationItem, NotificationListComponent} from '../../shared/ui/notification-list/notification-list.component';
 
 @Component({
   selector: 'app-student-page',
@@ -10,6 +11,7 @@ import {TaskCardComponent} from '../../shared/ui/task-card/task-card.component';
     HeaderComponent,
     TabsComponent,
     TaskCardComponent,
+    NotificationListComponent,
   ],
   templateUrl: './student-page.component.html',
   styleUrl: './student-page.component.less',
@@ -43,6 +45,52 @@ export class StudentPageComponent {
       dueText: '25 февраля 2026',
     },
   ];
+
+  notifications: NotificationItem[] = [
+    {
+      id: 1,
+      type: 'announcement',
+      title: 'Изменение расписания',
+      author: 'Смирнова Е.В.',
+      dateTime: '10 февраля 2026, 10:30',
+      text: 'Занятие в пятницу переносится на 16:00',
+      tag: 'Объявление',
+      isNew: true,
+      isUnread: true,
+    },
+    {
+      id: 2,
+      type: 'task',
+      title: 'Новое задание: Эссе по грамматике',
+      author: 'Смирнова Е.В.',
+      dateTime: '8 февраля 2026, 14:00',
+      text: 'Преподаватель Смирнова Е.В. создал новое задание',
+      tag: 'Задание',
+      isNew: true,
+      isUnread: true,
+    },
+    {
+      id: 3,
+      type: 'comment',
+      title: 'Новый комментарий к вашей работе',
+      author: 'Смирнова Е.В.',
+      dateTime: '12 февраля 2026, 14:30',
+      text: "Преподаватель оставил комментарий к заданию 'Эссе по грамматике'",
+      tag: 'Комментарий',
+      isNew: false,
+      isUnread: false,
+    },
+  ];
+
+  onTabChange(tabId: string) {
+    this.activeTab = tabId;
+  }
+
+  markNotificationAsRead(notificationId: number) {
+    this.notifications = this.notifications.map((item) =>
+      item.id === notificationId ? { ...item, isUnread: false } : item,
+    );
+  }
 
   openTaskDetails(taskTitle: string) {
     console.log('details:', taskTitle);
