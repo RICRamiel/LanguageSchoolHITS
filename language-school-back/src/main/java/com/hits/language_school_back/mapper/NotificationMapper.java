1256055064aad32c4f3d78d5e0ff32f86bd91846
Vasilii Polushkin <kvvarta@yandex.ru>
2026-03-11 22:35:59 +0700

    attachments for notifications !!

--- language-school-back/src/main/java/com/hits/language_school_back/mapper/NotificationMapper.java
+++ language-school-back/src/main/java/com/hits/language_school_back/mapper/NotificationMapper.java
@@ -1,11 +1,14 @@
 package com.hits.language_school_back.mapper;
 
+import com.hits.language_school_back.dto.AttachmentDownloadInfo;
 import com.hits.language_school_back.dto.NotificationDto;
 import com.hits.language_school_back.dto.UserDTO;
 import com.hits.language_school_back.dto.UserFullDTO;
 import com.hits.language_school_back.model.Notification;
 import com.hits.language_school_back.model.User;
 import org.springframework.stereotype.Component;
+
+import static java.util.stream.Collectors.toList;
 
 @Component
 public class NotificationMapper {
@@ -16,6 +19,13 @@ public class NotificationMapper {
                 .createdByTeacherWithId(model.getCreatedBy().getId())
                 .creationDate(model.getCreationDate())
                 .groupId(model.getGroup().getId())
-                .build();
+                .attachments(model.getAttachmentList().stream().map(attachment ->
+                        AttachmentDownloadInfo.builder()
+                                .fileName(attachment.getFileName())
+                                .fileType(attachment.getFileType())
+                                .fileSize(attachment.getFileSize())
+                                .objectKey(attachment.getObjectKey())
+                                .build()
+                ).toList()).build();
     }
 }
